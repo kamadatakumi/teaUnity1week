74d09909df7ed9972bb5ecd60382b690affbef51
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2018-06-20 19:25:42 +0900

    fin

--- Assets/Script/GameDirector.cs
+++ Assets/Script/GameDirector.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameDirector : MonoBehaviour {
     GameObject car;
     GameObject flag;
-    GameObject range;
-    GameObject result;
+    TextMeshProUGUI range;
+    TextMeshProUGUI result;
     GameObject wave;
-    GameObject description;
+    TextMeshProUGUI description;
     float delta=0;
     float delta2 = 0;
     bool deltastart=false;
@@ -23,31 +24,33 @@ public class GameDirector : MonoBehaviour {
     bool audios=true;
     bool ranking=true;
     bool ranking2 = false;
-    GameObject rankingtext;
+    bool ranking3=false;
+    TextMeshProUGUI rankingtext;
     
 	// Use this for initialization
 	void Start () {
         car = GameObject.Find("FuzzyRed");
         flag = GameObject.Find("flag_med");
-        range = GameObject.Find("Range");
-        result = GameObject.Find("Result");
-        result.GetComponent<Text>().enabled = false;
+        range = GameObject.Find("Range").GetComponent<TextMeshProUGUI>();
+        result = GameObject.Find("Result").GetComponent<TextMeshProUGUI>();
+        result.GetComponent<TextMeshProUGUI>().enabled = false;
         wave = GameObject.Find("Wave2");
-        description = GameObject.Find("Description");
-        description.GetComponent<Text>().enabled = false;
+        description = GameObject.Find("Description").GetComponent<TextMeshProUGUI>();
+        description.GetComponent<TextMeshProUGUI>().enabled = false;
         AudioSource[] audioSources = GetComponents<AudioSource>();
         tin1 = audioSources[2];
         trumpet1 = audioSources[1];
-        rankingtext = GameObject.Find("Rankingtext");
-        rankingtext.GetComponent<Text>().enabled = false;
+        rankingtext = GameObject.Find("Rankingtext").GetComponent<TextMeshProUGUI>();
+        rankingtext.GetComponent<TextMeshProUGUI>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetSceneByName("Ranking").IsValid())  return; 
         float length = flag.transform.position.x - car.transform.position.x;
         if (length < 0) length *= -1;
-            range.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+            range.GetComponent<TextMeshProUGUI>().text = "ゴールまで" + length.ToString("F2") + "m";
 
         if (car.transform.position.y > wave.transform.position.y + 20)
         {
@@ -67,9 +70,9 @@ public class GameDirector : MonoBehaviour {
                     GetComponent<AudioSource>().PlayOneShot(tin1.clip);
                     audios = false;
                 }
-                range.GetComponent<Text>().enabled = false;
-                result.GetComponent<Text>().text = "結果:" + length.ToString("F2") + "m";
-                result.GetComponent<Text>().enabled = true;
+                range.GetComponent<TextMeshProUGUI>().enabled = false;
+                result.GetComponent<TextMeshProUGUI>().text = "結果:" + length.ToString("F2") + "m";
+                result.GetComponent<TextMeshProUGUI>().enabled = true;
                 deltastart2 = true;
                 ranking2 = true;
                 }
@@ -81,11 +84,11 @@ public class GameDirector : MonoBehaviour {
                 GetComponent<AudioSource>().PlayOneShot(trumpet1.clip);
                 audios = false;
             }
-            range.GetComponent<Text>().enabled = false;
-            result.GetComponent<Text>().text = "結果:失敗";
-            result.GetComponent<Text>().enabled = true;
+            range.GetComponent<TextMeshProUGUI>().enabled = false;
+            result.GetComponent<TextMeshProUGUI>().text = "結果:失敗";
+            result.GetComponent<TextMeshProUGUI>().enabled = true;
             deltastart2 = true;
-            ranking2 = true;
+            ranking3 = true;
         }
         if (deltastart2 == true)
         {
@@ -93,25 +96,35 @@ public class GameDirector : MonoBehaviour {
         }
         if (delta2 > spacetime2)
         {
-            description.GetComponent<Text>().enabled = true;
+            description.GetComponent<TextMeshProUGUI>().enabled = true;
             if (ranking2 == true)
             {
-                rankingtext.GetComponent<Text>().enabled = true;
-                if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+                rankingtext.GetComponent<TextMeshProUGUI>().enabled = true;
+                if (Input.GetKeyDown(KeyCode.T))
                 {
-                    naichilab.UnityRoomTweet.Tweet("Giri_Car", "Giri Carをやってみたよ。結果は "+length.ToString("F2")+"m でした。みんなもやってみない？", "unityroom", "unity1week");
+                    naichilab.UnityRoomTweet.Tweet("Giri_Car", "Giri Carをやってみたよ。結果は " + length.ToString("F2") + "m でした。みんなもやってみない？", "unityroom", "unity1week");
+                }
+                if (Input.GetKeyDown(KeyCode.Return)&&ranking2==true)
+                {
+                        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(length);
+                }
+            }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene("Title");
                 }
 
-                if (Input.GetKeyDown(KeyCode.Return))
+            if (ranking3 == true)
+            {
+                rankingtext.GetComponent<TextMeshProUGUI>().text = " 　　　　　　     　ツイート: T";
+                rankingtext.GetComponent<TextMeshProUGUI>().enabled = true;
+
+                if (Input.GetKeyDown(KeyCode.T))
                 {
-                    naichilab.RankingLoader.Instance.SendScoreAndShowRanking(length);
+                    naichilab.UnityRoomTweet.Tweet("Giri_Car", "Giri Carをやってみたよ。結果は 失敗 でした。みんなもやってみない？", "unityroom", "unity1week");
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Space))
-                {
-                SceneManager.LoadScene("Title");
-            }
         }
-        
+       
     }
 }
--- Assets/Script/StartGame.cs
+++ Assets/Script/StartGame.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class StartGame : MonoBehaviour {
-    GameObject titletext1;
-    GameObject titletext2;
-    GameObject titletext3;
+    TextMeshProUGUI titletext1;
+    TextMeshProUGUI titletext2;
+    TextMeshProUGUI titletext3;
     GameObject titleimage;
     float delta=0;
     bool key=false;
@@ -15,12 +16,12 @@ public class StartGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        titletext1 = GameObject.Find("Title1");
-        titletext2 = GameObject.Find("Title2");
-        titletext3 = GameObject.Find("Description");
-        titleimage= GameObject.Find("TitleDes");
+        titletext1 = GameObject.Find("Title1").GetComponent<TextMeshProUGUI>();
+        titletext2 = GameObject.Find("Title2").GetComponent<TextMeshProUGUI>();
+        titletext3 = GameObject.Find("Description").GetComponent<TextMeshProUGUI>();
+        titleimage = GameObject.Find("TitleDes");
 
-        titletext3.GetComponent<Text>().enabled = false;
+        titletext3.GetComponent<TextMeshProUGUI>().enabled = false;
         titleimage.GetComponent<Image>().enabled = false;
     }
 	
@@ -28,9 +29,9 @@ public class StartGame : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            titletext1.GetComponent<Text>().enabled = false;
-            titletext2.GetComponent<Text>().enabled = false;
-            titletext3.GetComponent<Text>().enabled = true;
+            titletext1.GetComponent<TextMeshProUGUI>().enabled = false;
+            titletext2.GetComponent<TextMeshProUGUI>().enabled = false;
+            titletext3.GetComponent<TextMeshProUGUI>().enabled = true;
             titleimage.GetComponent<Image>().enabled = true;
 
             key = true;
